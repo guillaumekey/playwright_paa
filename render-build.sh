6820e78f1c83207ec433a71f27a46f3bdfaa4c55
guillaumekey <guillaumep@keyweo.com>
2026-02-25 17:37:35 +0100

    V0.1

--- render-build.sh
+++ render-build.sh
@@ -2,5 +2,13 @@
 # exit on error
 set -o errexit
 
+# Installation des dépendances Python
 pip install -r requirements.txt
-playwright install chromium
+
+# Installation de Playwright et de ses navigateurs
+# On force l'installation de Chromium spécifiquement
+python -m playwright install chromium
+
+# OPTIONNEL : Si Render se plaint encore de librairies manquantes (.so), 
+# décommente la ligne suivante (attention, peut être lent au build)
+# python -m playwright install-deps chromium
